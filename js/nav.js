8370a9e4d081638794f8f07594dc7bfddbddb123
ltur319 <ltur319@wgu.edu>
2026-02-19 23:23:57 -0800

    setup nav bar injection and code to dynamically change color of active page on navbar

---
+++ js/nav.js
@@ -0,0 +1,33 @@
+document.getElementById("navbar").innerHTML = `
+  <nav id="navbar">
+    <a class="nav-logo" href="/index.html">
+      AstroQuest
+    </a>
+    <ul>
+      <li>
+        <a href="/pages/explore/explore.html">Explore</a>
+      </li>
+      <li>
+        <a href="/pages/track/track.html">Track</a>
+      </li>
+      <li>
+        <a href="/pages/discover/discover.html">Discover</a>
+      </li>
+      <li>
+        <a href="/pages/learn/learn.html">Learn</a>
+      </li>
+    </ul>
+  </nav>`;
+
+const navLinks = document.querySelectorAll("ul li a");
+const currentPath = window.location.pathname.split("/").pop();
+
+navLinks.forEach((link) => {
+  const linkPath = link.href.split("/").pop();
+  if (
+    currentPath === linkPath ||
+    (currentPath === "" && linkPath === "index.html")
+  ) {
+    link.classList.add("active");
+  }
+});
