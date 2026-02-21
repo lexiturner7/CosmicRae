d77ec75d7a9257d6174bbc222a52a8fb82b7291c
ltur319 <ltur319@wgu.edu>
2026-02-20 21:28:00 -0800

    finished styling iss page

--- js/iss.js
+++ js/iss.js
@@ -1,11 +1,14 @@
 // MAP FUNCTION
 
 var map = L.map("iss-map").setView([51.505, -0.09], 4);
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+L.tileLayer(
+  "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
+  {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  },
+).addTo(map);
 
 const marker = L.marker([51.5, -0.09]).addTo(map);
 
