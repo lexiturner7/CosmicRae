e3e6d3f52ce127e2532c4a32252ae6e50b11d0c4
ltur319 <ltur319@wgu.edu>
2026-04-06 14:50:27 -0400

    added pop-up data and photos on hover Artemis 2 timeline. added framework for footer.

---
+++ js/footer.js
@@ -0,0 +1,50 @@
+document.getElementById("footer").innerHTML = 
+  <footer>
+    <h4 id="footer-title">CosmicRae</h4>
+    <span id="footer-tagline">Because the universe is for everyone</span>
+    <div id="footer-links">
+      {/* ************* PAGE LINKS *************** */}
+      <div id="page-links">
+        <ul id="footer-explore-links">
+          <span>Explore</span>
+          <li>
+            <a href="/pages/explore/apod.html">APOD</a>
+          </li>
+        </ul>
+
+        <ul id="footer-track-links">
+          <span>Track</span>
+          <li>
+            <a href="/pages/explore/iss.html">ISS</a>
+          </li>
+          <li>
+            <a href="/pages/explore/artemis2.html">Artemis II</a>
+          </li>
+          <li>
+            <a href="/pages/explore/asteroids.html">Asteroid Watch</a>
+          </li>
+        </ul>
+
+        <ul id="footer-discover-links">
+          <span>Discover</span>
+          <li>
+            <a href="/pages/explore/planets.html">Planets</a>
+          </li>
+          <li>
+            <a href="/pages/explore/issinfo.html">ISS Info</a>
+          </li>
+        </ul>
+      </div>
+
+      {/* ************* DATA SOURCES *************** */}
+
+      <div id="data-sources">
+        
+      </div>
+
+      {/* ************* ABOUT LINKS *************** */}
+
+      <div id="footer-about-links"></div>
+    </div>
+  </footer>
+);
