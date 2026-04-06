a931bc21e03459618de0e87c531633f187521a7f
ltur319 <ltur319@wgu.edu>
2026-04-06 16:23:03 -0400

    finished footer and added to all pages

--- js/footer.js
+++ js/footer.js
@@ -1,9 +1,11 @@
-document.getElementById("footer").innerHTML = 
+document.getElementById("footer").innerHTML = `
   <footer>
-    <h4 id="footer-title">CosmicRae</h4>
+  <div id="footer-top">
+  <div id="footer-brand">  
+  <h4 id="footer-title">CosmicRae</h4>
     <span id="footer-tagline">Because the universe is for everyone</span>
+    </div>
     <div id="footer-links">
-      {/* ************* PAGE LINKS *************** */}
       <div id="page-links">
         <ul id="footer-explore-links">
           <span>Explore</span>
@@ -15,36 +17,81 @@ document.getElementById("footer").innerHTML =
         <ul id="footer-track-links">
           <span>Track</span>
           <li>
-            <a href="/pages/explore/iss.html">ISS</a>
+            <a href="/pages/track/iss.html">ISS</a>
           </li>
           <li>
-            <a href="/pages/explore/artemis2.html">Artemis II</a>
+            <a href="/pages/track/artemis2.html">Artemis II</a>
           </li>
           <li>
-            <a href="/pages/explore/asteroids.html">Asteroid Watch</a>
+            <a href="/pages/track/asteroids.html">Asteroid Watch</a>
           </li>
         </ul>
 
         <ul id="footer-discover-links">
           <span>Discover</span>
           <li>
-            <a href="/pages/explore/planets.html">Planets</a>
+            <a href="/pages/discover/planets.html">Planets</a>
           </li>
           <li>
-            <a href="/pages/explore/issinfo.html">ISS Info</a>
+            <a href="/pages/discover/issinfo.html">ISS Info</a>
           </li>
         </ul>
       </div>
 
-      {/* ************* DATA SOURCES *************** */}
+      <div id="footer-data-sources">
+      <ul id="footer-data-links">
+          <span>Data Sources</span>
+          <li>
+            <a href="https://api.nasa.gov" target="_blank">NASA APOD API</a>
+          </li>
+          <li>
+            <a href="https://api.nasa.gov" target="_blank">NASA NeoWs API</a>
+          </li>
+          <li>
+            <a href="https://wheretheiss.at" target="_blank">wheretheiss.at</a>
+          </li>
+          <li>
+            <a href="https://github.com/corquaid/international-space-station-APIs" target="_blank">corquaid ISS Crew</a>
+          </li>
+          <li>
+            <a href="https://www.nasa.gov/missions/artemis/artemis-2/track-nasas-artemis-ii-mission-in-real-time/" target="_blank">NASA/JSC Ephemeris</a>
+          </li>
+          <li>
+            <a href="https://leafletjs.com" target="_blank">Leaflet.js</a>
+          </li>
+          <li>
+            <a href="https://www.chartjs.org" target="_blank">Chart.js</a>
+          </li>
+          <li>
+            <a href="https://images.nasa.gov" target="_blank">NASA Imagery</a>
+          </li>
+        </ul>
+        </div>
+      
 
-      <div id="data-sources">
+      <div id="footer-contact-links">
+        <span>Contact</span>
+        
+          <a href="https://github.com/lexiturner7/CosmicRae" target="_blank">
+            GitHub
+          </a>
+          <a
+            href="https://www.linkedin.com/in/lexi-turner-profile/"
+            target="_blank"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="mailto:lexi.turner7@gmail.com"
+            target="_blank"
+          >
+            Email
+          </a>
         
       </div>
-
-      {/* ************* ABOUT LINKS *************** */}
-
-      <div id="footer-about-links"></div>
     </div>
-  </footer>
-);
+
+    </div>
+
+    <div id="copyright">© 2026 Lexi Turner · CosmicRae</div>
+  </footer>`;
